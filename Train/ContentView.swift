7e196827aa410c3288415a2b9ca24a12d77b5ab0
Sean <spm317@gmail.com>
2025-04-02 19:18:35 -0400

    Initial Commit

---
+++ Train/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Train
+//
+//  Created by Sean Maloney on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Train/TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainApp.swift
+//  Train
+//
+//  Created by Sean Maloney on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
